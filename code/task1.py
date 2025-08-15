a65c8fa9870bfa4ed092902f07fa97f2808deb3d
prajasunil17 <69504834+prajasunil17@users.noreply.github.com>
2025-08-15 23:14:49 +0530

    Add files via upload

---
+++ code/task1.py
@@ -0,0 +1,16 @@
+param1=input("Enter the first number:")
+param2=input("Enter the second number:")
+
+param1 = int(param1)
+param2 = int(param2)
+
+add=param1+param2
+sub=param1-param2
+mult=param1*param2
+div=param1/param2
+
+
+print("\nAddition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mult)
+print("Division: ",div)
